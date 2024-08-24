711b047dc13e118adecdf4533e6c08097693abe5
Abhishek Baranwal <baranwalabhishek21@gmail.com>
2024-08-25 01:47:19 +0530

    Landing page bootstrapped

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        title: ['Merriweather','serif'],
+        subtitle: [ "Baskervville SC", 'serif']
+      },
     },
   },
   plugins: [],
